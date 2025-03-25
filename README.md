0b20f8bd5dd0f11bf7b3b98257a4e608d4809c4b
mani <mani@mac.mynetworksettings.com>
2025-03-24 21:29:58 -0400

    added readmefile

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# aws-terraform
+## Terraform steps
+
+```bash 
+
+cd aws-terraform 
+
+terraform init
+
+terraform plan
+
+terraform fmt
+
+terraform validate
+
+terraform apply 
+
+terraform destroy
+
+```
+
+
--- main.tf
+++ main.tf
@@ -18,34 +18,34 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "terraform-state-file-training"
-    key    = "ec2/101/state"
-    region = "us-east-2"
+    bucket  = "terraform-state-file-training"
+    key     = "ec2/101/state"
+    region  = "us-east-2"
     profile = "mani-aws"
   }
 }
 
 resource "aws_instance" "app" {
-  count =  var.number_of_app_instance
-  ami           = var.instance_ami 
+  count         = var.number_of_app_instance
+  ami           = var.instance_ami
   instance_type = var.app_instance_type
 
   tags = {
-    Name = "maniAppserver"
+    Name  = "maniAppserver"
     Owner = "mani"
-    Team  =  "Something"
+    Team  = "Something"
   }
 }
 
 resource "aws_instance" "db" {
-  count =  var.number_of_db_instance
-  ami           = var.instance_ami 
+  count         = var.number_of_db_instance
+  ami           = var.instance_ami
   instance_type = var.db_instance_type
 
   tags = {
-    Name = "maniAppserver"
+    Name  = "maniAppserver"
     Owner = "mani"
-    Team  =  "Something"
+    Team  = "Something"
   }
 }
 
