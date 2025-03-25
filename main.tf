6f06a478e614f35d247a1656f669c9f873bd635f
mani <mani@mac.mynetworksettings.com>
2025-03-24 21:26:54 -0400

    day-4

--- main.tf
+++ main.tf
@@ -15,10 +15,20 @@ provider "aws" {
   profile = "mani-aws"
 }
 
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-file-training"
+    key    = "ec2/101/state"
+    region = "us-east-2"
+    profile = "mani-aws"
+  }
+}
+
 resource "aws_instance" "app" {
-  count =  3
-  ami           = "ami-0d0f28110d16ee7d6"
-  instance_type = "t2.micro"
+  count =  var.number_of_app_instance
+  ami           = var.instance_ami 
+  instance_type = var.app_instance_type
 
   tags = {
     Name = "maniAppserver"
@@ -26,5 +36,18 @@ resource "aws_instance" "app" {
     Team  =  "Something"
   }
 }
+
+resource "aws_instance" "db" {
+  count =  var.number_of_db_instance
+  ami           = var.instance_ami 
+  instance_type = var.db_instance_type
+
+  tags = {
+    Name = "maniAppserver"
+    Owner = "mani"
+    Team  =  "Something"
+  }
+}
+
 
 
